cc95bb35f7407b3ec9d3648eb6f9e718e9c4dd36
ranggakrisnaa <infinity.duos@gmail.com>
2025-11-11 12:34:02 +0700

    feat: add setup all cors origin

--- internal/router/router.go
+++ internal/router/router.go
@@ -11,6 +11,17 @@ import (
 )
 
 func Register(app *fiber.App, articleHandler *article.Handler) {
+	// CORS
+	app.Use(func(c *fiber.Ctx) error {
+		c.Set("Access-Control-Allow-Origin", "*")
+		c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Method() == "OPTIONS" {
+			return c.SendStatus(fiber.StatusOK)
+		}
+		return c.Next()
+	})
+
 	// Global middlewares
 	app.Use(recover.New())
 	app.Use(func(c *fiber.Ctx) error {
